d3a24c5c3363ec513a59b983b89fbcf9060aed2a
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-06-24 21:42:17 +0300

    fix: skip collectstatic in dev mode, remove hardcoded ALLOWED_HOSTS override

--- smartstock-backend/config/settings/development.py
+++ smartstock-backend/config/settings/development.py
@@ -2,8 +2,6 @@ from .base import *  # noqa: F403
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'  # noqa: F405
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
